37d54ccb381052683347fd8ef5bbde71cf6bd22a
Koushik099 <kou.saha.ami@gmail.com>
2022-09-20 22:20:29 +0530

    ready to deploy

---
+++ js/main.js
@@ -0,0 +1,101 @@
+let rock = document.getElementById("r");
+let paper = document.getElementById("p");
+let scissor = document.getElementById("s");
+
+let userScore = document.getElementById("userScoreVal");
+let comScore = document.getElementById("compScoreVal");
+let userCounter = 0;
+let comCounter = 0;
+userScore.innerText = userCounter;
+comScore.innerText = comCounter;
+
+let resultShow = document.getElementById("result-final-stat");
+let userinput = document.getElementById("result-user-stat");
+let comInput = document.getElementById("result-comp-stat");
+
+let computerGenChoice = () => {
+  let str = "rps";
+  let val = "";
+  val += str[Math.floor(Math.random() * 3)];
+  console.log(val);
+  return val;
+};
+
+let matchTied = () => {
+  resultShow.textContent = "Winner: Tied! 😶";
+};
+
+let winnerUser = () => {
+  userCounter++;
+  userScore.innerText = userCounter;
+  resultShow.textContent = "Winner: User 🥇";
+};
+
+let winnerCom = () => {
+  comCounter++;
+  comScore.innerText = comCounter;
+  resultShow.textContent = "Winner: Robot 🥇";
+};
+
+rock.addEventListener("click", () => {
+  let value = computerGenChoice();
+  let user = "r";
+  if (user === value) {
+    matchTied();
+    // alert("Tied!. 🤔");
+  } else if (user == "r" && value == "s") {
+    userinput.innerText = `User: Rock`;
+    comInput.innerText = `Computer: Scissor`;
+
+    winnerUser();
+    // alert("User won ! 🥇");
+  } else if (user == "r" && value == "p") {
+    userinput.innerText = `User: Rock`;
+    comInput.innerText = `Computer: Paper`;
+
+    winnerCom();
+    // alert("Robot won ! 🥇");
+  }
+});
+
+paper.addEventListener("click", () => {
+  let value = computerGenChoice();
+  let user = "p";
+  if (user === value) {
+    matchTied();
+    // alert("Tied!. 🤔");
+  } else if (user == "p" && value == "r") {
+    userinput.innerText = `User: Paper`;
+    comInput.innerText = `Computer: Rock`;
+
+    winnerUser();
+    // alert("User won ! 🥇");
+  } else if (user == "p" && value == "s") {
+    userinput.innerText = `User: Paper`;
+    comInput.innerText = `Computer: Scissor`;
+
+    winnerCom();
+    // alert("Robot won ! 🥇");
+  }
+});
+
+scissor.addEventListener("click", () => {
+  let value = computerGenChoice();
+  let user = "s";
+  if (user === value) {
+    matchTied();
+    // alert("Tied!. 🤔");
+  } else if (user == "s" && value == "p") {
+    userinput.innerText = `User: Scissor`;
+    comInput.innerText = `Computer: Paper`;
+
+    winnerUser();
+    // alert("User won ! 🥇");
+  } else if (user == "s" && value == "r") {
+    userinput.innerText = `User: Scissor`;
+    comInput.innerText = `Computer: Rock`;
+
+    winnerCom();
+    // alert("Robot won ! 🥇");
+  }
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# This is JS Rock Paper Scissors Game.
+
+I gave lots of effort to complete this Rock Paper Scissors Game. Using CSS3, HTML5, Bootstarp and JavaSCript, The new Utilities.
+
+# View this Project On Netlify, Here is the Link.
+
+**[RockPaperScissors](https://mynewrpsgame.netlify.app/)**
+
+# Here is the Screenshot, take a look.
+
+![Project-05](./Proj_Image/Project.png)
+
+**Thank You.**
